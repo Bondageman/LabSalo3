f77a19b0f25a729ecf6c1c9cbf0b12c3daeb307d
ІПЗ-31/1 <ipz-311@iMac-Administrator.local>
2025-10-03 11:10:22 +0300

    first commit

---
+++ Lab3Salo/Lab3Salo/FormData.swift
@@ -0,0 +1,9 @@
+//
+//  FormData.swift
+//  Lab3Salo
+//
+//  Created by ІПЗ-31/1 on 03.10.2025.
+//
+
+import Foundation
+
---
+++ Lab3Salo/Lab3Salo/FormViewController.swift
@@ -0,0 +1,15 @@
+//
+//  FormViewController.swift
+//  Lab3Salo
+//
+//  Created by ІПЗ-31/1 on 03.10.2025.
+//
+
+import Foundation
+import UIKit
+
+class FormViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
